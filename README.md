f622720c78a431fd86c2ed323871dccac4e5f1b5
Marcin Ignac <marcin.ignac@gmail.com>
2015-05-19 07:19:11 +0100

    Global halo color

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # JS/WebGL Ora SDK Client
+
+### Prerequisites
+
+This project requires NodeJS 0.11.*.
+
+### Installing dependencies
+
+To install 3rd party libraries run the following command in the project folder.
+
+```
+npm install
+```
+
+### Building
+
+```
+browserify app/main.js --ignore-missing=plask -g brfs -o main.web.js
+```
+
+After that you should be able to open index.html. Depending on your browser local content policy you might need to upload index.html, main.web.js and assets folder to a server for them to load properly.
--- app/main.js
+++ app/main.js
@@ -2,21 +2,19 @@ var Platform      = require('pex-sys').Platform;
 var Time          = require('pex-sys').Time;
 var Window        = require('pex-sys').Window;
 var Color         = require('pex-color').Color;
-var Geometry      = require('pex-geom').Geometry;
 var Vec3          = require('pex-geom').Vec3;
 var Vec2          = require('pex-geom').Vec2;
 var glu           = require('pex-glu');
-var Mesh          = require('pex-glu').Mesh;
-var Texture2D     = require('pex-glu').Texture2D;
 var Camera        = require('pex-glu').PerspectiveCamera;
 var Arcball       = require('pex-glu').Arcball;
-var SolidColor    = require('pex-materials').SolidColor;
-var Textured      = require('pex-materials').Textured;
-var ShowTexCoords = require('pex-materials').ShowTexCoords;
-
+var GUI           = require('pex-gui').GUI;
 var Halo          = require('ora-halo');
 
 var ASSETS_PATH = Platform.isPlask ? '../assets' : 'assets';
+
+var State = {
+  color: 0
+}
 
 Window.create({
   settings: {
@@ -28,8 +26,13 @@ Window.create({
   init: function() {
     this.halo = new Halo({
       lineTexture: ASSETS_PATH + '/textures/dots.png',
-      colorTexture: ASSETS_PATH + '/textures/calories-gradient.png'
+      colorTexture: ASSETS_PATH + '/textures/halo-gradient.png'
     });
+
+    this.gui = new GUI(this);
+    this.gui.addParam('Global Color', State, 'color', {}, function(value) {
+      this.halo.setGlobalParam('color', value);
+    }.bind(this));
 
     this.camera = new Camera(60, this.width / this.height);
     this.arcball = new Arcball(this, this.camera);
@@ -43,5 +46,7 @@ Window.create({
 
     this.gl.lineWidth(4);
     this.halo.draw(this.camera);
+
+    this.gui.draw();
   }
 });
